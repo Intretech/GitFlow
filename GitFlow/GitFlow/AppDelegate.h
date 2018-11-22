2027aca8e220d03d2650a6f27151d2f26fa84096
mohamedaslam28 <mohamedaslam28@gmail.com>
2018-11-22 09:51:01 +0800

    Gitflow initial commit

---
+++ GitFlow/GitFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitFlow
+//
+//  Created by intretech on 2018/11/22.
+//  Copyright © 2018 intretech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitFlow/GitFlow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitFlow
+//
+//  Created by intretech on 2018/11/22.
+//  Copyright © 2018 intretech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
